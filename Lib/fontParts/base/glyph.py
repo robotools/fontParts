8ebc1e34a2980f9aa336ea1db05540542b3583dc
Frederik Berlaen <frederik@typemytype.com>
2018-02-05 21:42:22 +0100

    adding self to call the method

--- Lib/fontParts/base/glyph.py
+++ Lib/fontParts/base/glyph.py
@@ -1400,7 +1400,7 @@ class BaseGlyph(BaseObject, TransformationMixin, DeprecatedGlyph, RemovedGlyph):
 
             >>> mg = glyph.toMathGlyph()
         """
-        return _toMathGlyph()
+        return self._toMathGlyph()
 
     def _toMathGlyph(self):
         mathGlyph = fontMath.MathGlyph(None)
@@ -1449,7 +1449,7 @@ class BaseGlyph(BaseObject, TransformationMixin, DeprecatedGlyph, RemovedGlyph):
 
         mathGlyph is the mathGlyph to put into the current glyph.
         """
-        return _fromMathGlyph(mathGlyph, toThisGlyph=True)
+        return self._fromMathGlyph(mathGlyph, toThisGlyph=True)
 
     def _fromMathGlyph(self, mathGlyph, toThisGlyph=False):
         # make the destination
