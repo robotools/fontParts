607c5caa901b9f12417d9c2d589a380c47d09030
Simon Cozens <simon@simon-cozens.org>
2019-11-18 16:17:04 +0000

    Fix glyph class inheritance

--- Lib/fontParts/fontshell/otf/font.py
+++ Lib/fontParts/fontshell/otf/font.py
@@ -2,7 +2,7 @@ from fontParts.fontshell.ttf.font import TTFont,TTLayer
 from fontParts.fontshell.otf.glyph import OTGlyph
 
 class OTLayer(TTLayer):
-    layerClass = OTGlyph
+    glyphClass = OTGlyph
 
 class OTFont(TTFont):
     layerClass = OTLayer
