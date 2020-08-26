a13b5f3dedf0103c50a641eab7bad708581ff27e
Frank Grießhammer <frankrolf@gmail.com>
2020-08-26 16:26:30 +0200

    Clarify documentation for kerning.find (#542)
    * Clarify documentation for kerning.find
    
    The docstring for kerning.get and kerning.find methods was the same, while they perform different operations. I hope this update clarifies the difference a bit.
    
    * Formatting
    
    Co-authored-by: Ben Kiel <ben@benkiel.com>

--- Lib/fontParts/base/kerning.py
+++ Lib/fontParts/base/kerning.py
@@ -351,7 +351,9 @@ class BaseKerning(BaseDict, DeprecatedKerning, RemovedKerning):
 
     def find(self, pair, default=None):
         """
-        Returns the value for the kerning pair.
+        Returns the value for the kerning pair - even if the pair only exists
+        implicitly (one or both sides may be members of a kerning group).
+
         **pair** is a ``tuple`` of two :ref:`type-string`\s, and the returned
         values will either be :ref:`type-int-float` or ``None``
         if no pair was found. ::
