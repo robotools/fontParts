4e2e5cb1c2367e5e1c2926878683cb1022a582bd
Knut Nergaard <knut.nergaard@gmail.com>
2024-08-12 17:14:07 +0200

    Recommitting due to error.

---
+++ Lib/fontParts/base/types.py
@@ -0,0 +1,32 @@
+from typing import Dict, Generic, Iterator, List, Set, Tuple, TypeVar, Union
+
+try:
+    from fontParts.base.info import BaseInfo
+    from fontParts.base.groups import BaseGroups
+    from fontParts.base.kerning import BaseKerning
+    from fontParts.base.features import BaseFeatures
+    from fontParts.base.lib import BaseLib
+    from fontParts.base.layer import BaseLayer
+    from fontParts.base.glyph import BaseGlyph
+    from fontParts.base.guideline import BaseGuideline
+except ImportError:
+    pass
+
+IntFloat = Union[int, float]
+ColorType = Tuple[(IntFloat,) * 4]
+CoordinateType = Tuple[IntFloat, IntFloat]
+FactorType = Union[float, Tuple[float, float]]
+TransformationMatrixType = Tuple[(IntFloat,) * 6]
+FontType = TypeVar('FontType', bound='BaseFont')
+InfoType = TypeVar('InfoType', bound='BaseInfo')
+GroupsType = TypeVar('GroupsType', bound='BaseGroups')
+KerningType = TypeVar('KerningType', bound='BaseKerning')
+FeaturesType = TypeVar('FeaturesType', bound='BaseFeatures')
+LibType = TypeVar('LibType', bound='BaseLib')
+LayerType = TypeVar('LayerType', bound='BaseLayer')
+GlyphType = TypeVar('GlyphType', bound='BaseGlyph')
+GuidelineType = TypeVar('GuidelineType', bound='BaseGuideline')
+KerningKey = Tuple[str, str]
+KerningDict = Dict[KerningKey, IntFloat]
+ReverseComponentMapping = Dict[str, Set[str]]
+CharacterMapping = Dict[int, List[str]]
