e7a2abe8bf7c9cabf3c117ff2da7a8dcf5262ff2
Knut Nergaard <knut.nergaard@gmail.com>
2024-10-27 22:21:54 +0100

    Revise `annotations.py` with new generic container types.

--- Lib/fontParts/base/annotations.py
+++ Lib/fontParts/base/annotations.py
@@ -1,47 +1,47 @@
 # pylint: disable=C0103, C0114
-
 from __future__ import annotations
 from typing import Dict, List, Protocol, Tuple, TypeVar, Union
 
 from fontTools.pens.basePen import AbstractPen
 from fontTools.pens.pointPen import AbstractPointPen
 
-# ------------
-# Type Aliases
-# ------------
+# Generic
+T = TypeVar('T')
+
+Pair = Tuple[T, T]
+Quadruple = Tuple[T, T, T, T]
+Sextuple = Tuple[T, T, T, T, T, T]
+CollectionType = Union[List[T], Tuple[T, ...]]
+PairCollectionType = Union[List[T], Pair[T]]
+QuadrupleCollectionType = Union[List[T], Quadruple[T]]
+SextupleCollectionType = Union[List[T], Sextuple[T]]
 
 # Builtins
-
-T = TypeVar('T')
-CollectionType = Union[List[T], Tuple[T, ...]]
 IntFloatType = Union[int, float]
 
-# FontTools
-
+# Pens
 PenType = AbstractPen
 PointPenType = AbstractPointPen
 
-# FontParts
-
-BoundsType = Tuple[IntFloatType, IntFloatType, IntFloatType, IntFloatType]
+# Mapping
 CharacterMappingType = Dict[int, Tuple[str, ...]]
-ColorType = Tuple[IntFloatType, IntFloatType, IntFloatType, IntFloatType]
-CoordinateType = Tuple[IntFloatType, IntFloatType]
-FactorType = Union[IntFloatType, Tuple[IntFloatType, IntFloatType]]
-InterpolatableType = TypeVar('InterpolatableType', bound='Interpolatable')
-KerningKeyType = Tuple[str, str]
-KerningDictType = Dict[KerningKeyType, IntFloatType]
 ReverseComponentMappingType = Dict[str, Tuple[str, ...]]
-ScaleType = Tuple[IntFloatType, IntFloatType]
-TransformationMatrixType = Tuple[
-    IntFloatType, IntFloatType, IntFloatType,
-    IntFloatType, IntFloatType, IntFloatType
-]
+
+# Transformation
+TransformationType = Union[IntFloatType, List[IntFloatType], Pair[IntFloatType]]
+
+# Interpolation
+InterpolatableType = TypeVar('InterpolatableType', bound='Interpolatable')
 
 
 class Interpolatable(Protocol):
     """Represent a protocol for interpolatable types."""
 
-    def __add__(self, other: InterpolatableType) -> InterpolatableType: ...
-    def __sub__(self, other: InterpolatableType) -> InterpolatableType: ...
-    def __mul__(self, other: FactorType) -> InterpolatableType: ...
+    def __add__(self, other: InterpolatableType) -> InterpolatableType:
+        ...
+
+    def __sub__(self, other: InterpolatableType) -> InterpolatableType:
+        ...
+
+    def __mul__(self, other: TransformationType) -> InterpolatableType:
+        ...
