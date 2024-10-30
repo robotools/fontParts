9df12da2e08279e9284786c6a75e1a3228f414cd
Knut Nergaard <knut.nergaard@gmail.com>
2024-10-30 19:04:12 +0100

    Resolve merge conflicts.

--- Lib/fontParts/base/annotations.py
+++ Lib/fontParts/base/annotations.py
@@ -1,47 +1,47 @@
 # pylint: disable=C0103, C0114
+
 from __future__ import annotations
 from typing import Dict, List, Protocol, Tuple, TypeVar, Union
 
 from fontTools.pens.basePen import AbstractPen
 from fontTools.pens.pointPen import AbstractPointPen
 
-# Generic
-T = TypeVar('T')
-
-Pair = Tuple[T, T]
-Quadruple = Tuple[T, T, T, T]
-Sextuple = Tuple[T, T, T, T, T, T]
-CollectionType = Union[List[T], Tuple[T, ...]]
-PairCollectionType = Union[List[T], Pair[T]]
-QuadrupleCollectionType = Union[List[T], Quadruple[T]]
-SextupleCollectionType = Union[List[T], Sextuple[T]]
+# ------------
+# Type Aliases
+# ------------
 
 # Builtins
+
+T = TypeVar('T')
+CollectionType = Union[List[T], Tuple[T, ...]]
 IntFloatType = Union[int, float]
 
-# Pens
+# FontTools
+
 PenType = AbstractPen
 PointPenType = AbstractPointPen
 
-# Mapping
+# FontParts
+
+BoundsType = Tuple[IntFloatType, IntFloatType, IntFloatType, IntFloatType]
 CharacterMappingType = Dict[int, Tuple[str, ...]]
-ReverseComponentMappingType = Dict[str, Tuple[str, ...]]
-
-# Transformation
-TransformationType = Union[IntFloatType, List[IntFloatType], Pair[IntFloatType]]
-
-# Interpolation
+ColorType = Tuple[IntFloatType, IntFloatType, IntFloatType, IntFloatType]
+CoordinateType = Tuple[IntFloatType, IntFloatType]
+FactorType = Union[IntFloatType, Tuple[IntFloatType, IntFloatType]]
 InterpolatableType = TypeVar('InterpolatableType', bound='Interpolatable')
+KerningKeyType = Tuple[str, str]
+KerningDictType = Dict[KerningKeyType, IntFloatType]
+ReverseComponentMappingType = Dict[str, Tuple[str, ...]]
+ScaleType = Tuple[IntFloatType, IntFloatType]
+TransformationMatrixType = Tuple[
+    IntFloatType, IntFloatType, IntFloatType,
+    IntFloatType, IntFloatType, IntFloatType
+]
 
 
 class Interpolatable(Protocol):
     """Represent a protocol for interpolatable types."""
 
-    def __add__(self, other: InterpolatableType) -> InterpolatableType:
-        ...
-
-    def __sub__(self, other: InterpolatableType) -> InterpolatableType:
-        ...
-
-    def __mul__(self, other: TransformationType) -> InterpolatableType:
-        ...
+    def __add__(self, other: InterpolatableType) -> InterpolatableType: ...
+    def __sub__(self, other: InterpolatableType) -> InterpolatableType: ...
+    def __mul__(self, other: FactorType) -> InterpolatableType: ...
--- documentation/source/environments/objects/base.rst
+++
@@ -1,7 +0,0 @@
-.. highlight:: python
-.. module:: fontParts.base.base
-
-
-.. autoclass:: BaseObject
-    :members:
-    :special-members:
