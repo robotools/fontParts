0c555a3e58ada9732a14daff672e7900bdbffa8c
knutnergaard <knut.nergaard@gmail.com>
2025-03-17 09:25:29 +0100

    Annotate more modules.

--- Lib/fontParts/fontshell/info.py
+++ Lib/fontParts/fontshell/info.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Any
+
 import defcon
 from fontParts.base import BaseInfo
 from fontParts.fontshell.base import RBaseObject
@@ -6,8 +9,8 @@ from fontParts.fontshell.base import RBaseObject
 class RInfo(RBaseObject, BaseInfo):
     wrapClass = defcon.Info
 
-    def _getAttr(self, attr):
+    def _getAttr(self, attr: str) -> Any:
         return getattr(self.naked(), attr)
 
-    def _setAttr(self, attr, value):
+    def _setAttr(self, attr: str, value: Any) -> None:
         setattr(self.naked(), attr, value)
--- Lib/fontParts/fontshell/kerning.py
+++ Lib/fontParts/fontshell/kerning.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+from typing import Any
+from collections.abc import ItemsView
+
 import defcon
+from fontParts.base.annotations import PairCollectionType
 from fontParts.base import BaseKerning
 from fontParts.fontshell.base import RBaseObject
 
@@ -6,20 +11,26 @@ from fontParts.fontshell.base import RBaseObject
 class RKerning(RBaseObject, BaseKerning):
     wrapClass = defcon.Kerning
 
-    def _items(self):
-        return self.naked().items()
+    def _getNaked(self) -> defcon.Kerning:
+        kerning = self.naked()
+        if kerning is None:
+            raise ValueError("Kerning cannot be None.")
+        return kerning
 
-    def _contains(self, key):
-        return key in self.naked()
+    def _items(self) -> ItemsView[str, int]:
+        return self._getNaked().items()
 
-    def _setItem(self, key, value):
-        self.naked()[key] = value
+    def _contains(self, key: str) -> bool:
+        return key in self._getNaked()
 
-    def _getItem(self, key):
-        return self.naked()[key]
+    def _setItem(self, key: str, value: int) -> None:
+        self._getNaked()[key] = value
 
-    def _delItem(self, key):
-        del self.naked()[key]
+    def _getItem(self, key: str) -> Any:
+        return self._getNaked()[key]
 
-    def _find(self, pair, default=0):
-        return self.naked().find(pair, default)
+    def _delItem(self, key: str) -> None:
+        del self._getNaked()[key]
+
+    def _find(self, pair: PairCollectionType[str], default: int = 0) -> int:
+        return self._getNaked().find(pair, default)
