8ab7fc68d60efc0d3543b4e890eb37134437d2da
knutnergaard <knut.nergaard@gmail.com>
2025-05-30 10:41:06 +0200

    Document and resolve `LibValueType`.

--- Lib/fontParts/base/annotations.py
+++ Lib/fontParts/base/annotations.py
@@ -44,6 +44,25 @@ LibValueType = Union[
     datetime.datetime,
 ]
 
+class LibValue:
+    # Documentation class for LibValueType
+    """A :class:`~fontParts.base.BaseLib` value may be one of the following 
+    *non-collection* types:
+
+    - :class:`str` 
+    - :class:`int` 
+    - :class:`float` 
+    - :class:`bool`
+    - :class:`bytes`,
+    - :class:`bytearray` 
+    - :class:`datetime.datetime`
+
+    In addition, a value may also be a :class:`list` or :class:`tuple` containing any of 
+    the types above, or a :class:`dict` mapping :class:`str` keys to values of those 
+    same types (including nested lists, tuples, or dicts).
+
+    """
+
 # Transformation
 TransformationType = Union[IntFloatType, List[IntFloatType], PairType[IntFloatType]]
 
