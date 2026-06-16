744fcd5f7875f461141ec1a4ff4175336d8b9941
Knut Nergaard <knut.nergaard@gmail.com>
2026-06-16 15:49:32 +0200

    Make Color consistent with `Bounds` (#913)
    * Make Color consistent with Bounds
    
    - adopt named-tuple-based pattern
    - improve documentation
    - improve typing with signature overloads
    
    * Update test_color
    
    * Correct copy-paste error

--- Lib/fontParts/base/color.py
+++ Lib/fontParts/base/color.py
@@ -1,45 +1,49 @@
 from __future__ import annotations
+from collections import namedtuple
+from typing import overload, Any
 
 from fontParts.base.normalizers import normalizeColor
 from fontParts.base.annotations import IntFloatType, RGBALike
 
+_BaseColor = namedtuple("_BaseColor", ["r", "g", "b", "a"])
 
-class Color(tuple):
+
+class Color(_BaseColor):
     """Represent a color object following the :ref:`type-color`.
 
-    This class takes either individual RGBA component values or a :class:`list`
-    or :class:`tuple` of values.
+    This class accepts either four RGBA component values or a :class:`list` or
+    :class:`tuple` of these values.
+
+    :param r: The color's red component as :class:`int` or :class:`float`.
+    :param g: The color's green component as :class:`int` or :class:`float`.
+    :param b: The color's blue component as :class:`int` or :class:`float`.
+    :param a: The color's alpha component as :class:`int` or :class:`float`.
 
     """
 
-    def __new__(cls, *args: IntFloatType | RGBALike) -> Color:
-        value = args[0] if len(args) == 1 else args
+    __slots__ = ()
+
+    @overload
+    def __new__(
+        cls, r: IntFloatType, g: IntFloatType, b: IntFloatType, a: IntFloatType
+    ) -> Color: ...
+
+    @overload
+    def __new__(cls, color: RGBALike) -> Color: ...
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Color:
+        if kwargs:
+            value = (
+                kwargs.get("r", 0),
+                kwargs.get("g", 0),
+                kwargs.get("b", 0),
+                kwargs.get("a", 1),
+            )
+
+        else:
+            value = args[0] if len(args) == 1 else args
         normalizedValue = normalizeColor(value)  # type: ignore[arg-type]
+        return super().__new__(cls, *normalizedValue)
 
-        return super().__new__(cls, normalizedValue)
-
-    def __repr__(self):
-        return (
-            f"<{__class__.__name__} r={self.r}, g={self.g}, "
-            f"b={self.b}, a={self.a} at {id(self)}>"
-        )
-
-    @property
-    def r(self) -> IntFloatType:
-        """The color's red component as an :class:`int` or a :class:`float`."""
-        return self[0]
-
-    @property
-    def g(self) -> IntFloatType:
-        """The color's green component as an :class:`int` or a :class:`float`."""
-        return self[1]
-
-    @property
-    def b(self) -> IntFloatType:
-        """The color's blue component as an :class:`int` or a :class:`float`."""
-        return self[2]
-
-    @property
-    def a(self) -> IntFloatType:
-        """The color's alpha component as an :class:`int` or a :class:`float`."""
-        return self[3]
+    # Remove auto-generated _BaseCpøpr.__new__ docstring
+    __new__.__doc__ = ""
