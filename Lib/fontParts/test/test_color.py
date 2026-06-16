d5e478203029c157d4c6a3b81307cc0b5a3dc348
knutnergaard <knut.nergaard@gmail.com>
2026-06-16 15:44:04 +0200

    Correct copy-paste error

--- Lib/fontParts/test/test_color.py
+++ Lib/fontParts/test/test_color.py
@@ -4,22 +4,22 @@ from fontParts.base.color import Color
 
 class TestColor(unittest.TestCase):
     def test_new_tuple(self):
-        bounds = Color((0.1, 0.2, 0.3, 1.0))
-        self.assertEqual(bounds.r, 0.1)
-        self.assertEqual(bounds.g, 0.2)
-        self.assertEqual(bounds.b, 0.3)
-        self.assertEqual(bounds.a, 1.0)
+        color = Color((0.1, 0.2, 0.3, 1.0))
+        self.assertEqual(color.r, 0.1)
+        self.assertEqual(color.g, 0.2)
+        self.assertEqual(color.b, 0.3)
+        self.assertEqual(color.a, 1.0)
 
     def test_new_args(self):
-        bounds = Color((0.1, 0.2, 0.3, 1.0))
-        self.assertEqual(bounds.r, 0.1)
-        self.assertEqual(bounds.g, 0.2)
-        self.assertEqual(bounds.b, 0.3)
-        self.assertEqual(bounds.a, 1.0)
+        color = Color((0.1, 0.2, 0.3, 1.0))
+        self.assertEqual(color.r, 0.1)
+        self.assertEqual(color.g, 0.2)
+        self.assertEqual(color.b, 0.3)
+        self.assertEqual(color.a, 1.0)
 
     def test_new_kwargs(self):
-        bounds = Color(r=0.1, g=0.2, b=0.3, a=1.0)
-        self.assertEqual(bounds.r, 0.1)
-        self.assertEqual(bounds.g, 0.2)
-        self.assertEqual(bounds.b, 0.3)
-        self.assertEqual(bounds.a, 1.0)
+        color = Color(r=0.1, g=0.2, b=0.3, a=1.0)
+        self.assertEqual(color.r, 0.1)
+        self.assertEqual(color.g, 0.2)
+        self.assertEqual(color.b, 0.3)
+        self.assertEqual(color.a, 1.0)
